d87d3b9ab4d2cc26d9c0fa70295687ad4648094b
Alia <you@example.com>
2024-06-15 14:53:04 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* Інші компоненти вашого додатку */}
+    </div>
+  );
+}
+
+export default App;
