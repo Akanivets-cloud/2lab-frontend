c421d57e1899afac46d35ffdb2c4bf6d4b009655
Alia <you@example.com>
2024-06-17 23:24:51 +0300

    add alt

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Navbar, Nav, FormControl, Container, Form, Button } from 'react-bootstrap';
+import React, { Component, useState } from 'react';
+import { Navbar, Nav, FormControl, Container, Form, Button, Modal } from 'react-bootstrap';
 import logo from '../logo.svg';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from '../Pages/Home';
@@ -7,51 +7,82 @@ import Contacts from '../Pages/Contacts';
 import About from '../Pages/About';
 import Blog from '../Pages/Blog';
 
-export default class Header extends Component {
-  render() {
-    return (
-      <Router>
-        <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand as={Link} to="/">
-              <img
-                src={logo}
-                height="30"
-                width="30"
-                className="d-inline-block align-top"
-                alt="Logo"
-              />
-              React Site
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link as={Link} to="/">Home</Nav.Link>
-                <Nav.Link as={Link} to="/about">About us</Nav.Link>
-                <Nav.Link as={Link} to="/contacts">Contacts</Nav.Link>
-                <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
-              </Nav>
-              <Form className="d-flex">
-                <FormControl
-                  type="text"
-                  placeholder="Search"
-                  className="me-sm-3"
-                />
-                <Button variant="outline-info">Search</Button>
-              </Form>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+export default function Header() {
+  const [show, setShow] = useState(false);
 
-        <div style={{ paddingTop: '56px' }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contacts" element={<Contacts />} />
-            <Route path="/blog" element={<Blog />} />
-          </Routes>
-        </div>
-      </Router>
-    );
-  }
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <Router>
+      <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <img
+              src={logo}
+              height="30"
+              width="30"
+              className="d-inline-block align-top"
+              alt="Logo"
+            />
+            React Site
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/about">About us</Nav.Link>
+              <Nav.Link as={Link} to="/contacts">Contacts</Nav.Link>
+              <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+            </Nav>
+            <Button className="ms-2" onClick={handleShow}>Login</Button>
+            <Form className="d-flex">
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="me-sm-3"
+              />
+              <Button variant="outline-info">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Log in</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="fromBasicEmail">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="fromBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Enter password" />
+            </Form.Group>
+            <Form.Group controlId="fromBasicCheckbox">
+              <Form.Check type="checkbox" label="Remember me" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+
+      <div style={{ paddingTop: '56px' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
